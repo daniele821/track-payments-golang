97e7a4a6e12c68a7aa5136d83b8e338fcbe5c940
Daniele Muffato <danixgithub1@gmail.com>
2025-02-02 01:26:50 +0100

    updated gitignore

--- server/server.py
+++ server/server.py
@@ -2,8 +2,10 @@
 
 import sqlite3
 import os
+import tempfile
 
 FILE_NAME = __file__
 DIR_NAME = os.path.dirname(FILE_NAME)
+DB_FILE = DIR_NAME + "/.payments.db"
 
-sqlite3.connect(DIR_NAME + "/payments.db")
+sqlite3.connect(DB_FILE)
