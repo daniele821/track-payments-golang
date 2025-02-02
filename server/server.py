ea75561cd86e8106202ea4c0476327e54c31a6a6
Daniele Muffato <danixgithub1@gmail.com>
2025-02-02 19:42:00 +0100

    add populating

--- server/server.py
+++ server/server.py
@@ -3,6 +3,21 @@
 from database import Db
 
 db = Db("test")
+try:
+    db.insertCity("Asti")
+    db.insertCity("Cesena")
+    db.insertItem("Pane")
+    db.insertItem("Briosche")
+    db.insertShop("Coop")
+    db.insertShop("Conad")
+    db.insertShop("Paninaro")
+    db.insertMethod("Contante")
+    db.insertMethod("Postepay")
+    db.insertMethod("SanPaolo")
+    db.insertPayment("2025-01-01 12:34:32", "Asti", "Coop", "Contante")
+    db.insertDetail("Pane", 1, 3, 100)
+except Exception as e:
+    print(f'\x1b[1;31mFAILURE ---> {e}\x1b[m')
 print(db.getCity())
 print(db.getItem())
 print(db.getShop())
