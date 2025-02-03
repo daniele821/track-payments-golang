c43abccba8dce07b8bb9d18d18001fa41be3b571
Daniele Muffato <danixgithub1@gmail.com>
2025-02-03 21:20:18 +0100

    post requests handler (just simple placeholder. not really doing anything useful yet)

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@
 import http.server
 import threading
 import configs
+import json
 
 
 WEBSITE_DIR = configs.WEBSITE_DIR
@@ -12,6 +13,21 @@ FLAGS = configs.FLAGS
 class CustomHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=WEBSITE_DIR, **kwargs)
+
+    def do_POST(self):
+        # parse received data
+        content_length = int(self.headers.get("Content-Length", 0))
+        post_data = self.rfile.read(content_length).decode("utf-8")
+
+        response = json.loads(post_data)
+        
+        # TODO: THIS IS JUST A PLACEHOLDER FOR NOW, LOGIC IS STILL MISSING!
+
+        # Send a simple response
+        self.send_response(200)
+        self.send_header("Content-type", "text/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 
 def run_server():
