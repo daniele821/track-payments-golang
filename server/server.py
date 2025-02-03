8bddbcf47f5c81680bf1a8676675d9d14f25a4b5
Daniele Muffato <danixgithub1@gmail.com>
2025-02-03 10:40:46 +0100

    added function to open link

--- server/server.py
+++ server/server.py
@@ -1,30 +1,20 @@
 #!/bin/python3
 
-from database import Db
+import sys
+import os
 
-db = Db("test", True)
-try:
-    db.insertCity("Asti")
-    db.insertCity("Cesena")
-    db.insertItem("Pane")
-    db.insertItem("Briosche")
-    db.insertShop("Coop")
-    db.insertShop("Conad")
-    db.insertShop("Paninaro")
-    db.insertMethod("Contante")
-    db.insertMethod("Postepay")
-    db.insertMethod("SanPaolo")
-    db.insertPayment("2025-01-01 12:34:32", "Asti", "Coop", "Contante")
-    db.insertDetail("Pane", 1, 3, 100)
-    db.insertDetail("Briosche", 1, 3, 100)
-except Exception as e:
-    print(f"\x1b[1;31mFAILURE ---> {e}\x1b[m")
-print(db.getCity())
-print(db.getItem())
-print(db.getShop())
-print(db.getMethod())
-print(db.getPayment())
-print(db.getDetail())
-print()
-for line in db.getFullDetail():
-    print(line)
+def open_link(link):
+    if sys.platform == "linux":
+        if "TERMUX_VERSION" in os.environ:
+            os.system(f"termux-open {link}")
+        else:
+            os.system(f"xdg-open {link}")
+    elif sys.platform == "win32":
+        os.system(f"start {link}")
+    elif sys.platform == "darwin":
+        os.system(f"open {link}")
+    else:
+        raise Exception("unable to open link: unknown platform!")
+
+
+        
