cd9b85c8d2c1e54508854775f992ff2dc1df6289
Daniele Muffato <danixgithub1@gmail.com>
2025-02-04 14:43:12 +0100

    avoid multithreading

--- server/run.py
+++ server/run.py
@@ -1,15 +1,7 @@
 #!/bin/python3
 
 import server
-import database
-import utils
-import configs
 
-if not configs.FLAGS.noserver:
-    server_address = server.run_server()
-    print("started server on " + server_address)
-    if configs.FLAGS.gui:
-        utils.open_link(server_address)
-        print("launching browser to connect to the server...")
-    else:
-        print("not launching browser!")
+server.run()
+
+
--- server/server.py
+++ server/server.py
@@ -5,6 +5,10 @@ import threading
 import configs
 import json
 import database
+import sys
+import os
+import subprocess
+
 
 DB = database.Db("payments")
 
@@ -17,18 +21,42 @@ class CustomHTTPHandler(http.server.SimpleHTTPRequestHandler):
         content_length = int(self.headers.get("Content-Length", 0))
         post_data = self.rfile.read(content_length).decode("utf-8")
 
-        # execute query requested by json        
+        # execute query requested by json
         status_code, responseJson = DB.answerPostRequest(post_data)
-        
+
         # Send a simple response
         self.send_response(status_code)
         self.send_header("Content-type", "text/json")
         self.end_headers()
-        self.wfile.write(responseJson.encode('utf-8'))
+        self.wfile.write(responseJson.encode("utf-8"))
 
 
-def run_server():
-    server = http.server.HTTPServer(("localhost", configs.FLAGS.port), CustomHTTPHandler)
-    threading.Thread(target=server.serve_forever).start()
-    # http:// is necessary for termux-open to work
-    return "http://localhost:" + str(server.server_address[1])
+def open_link(link):
+    if sys.platform == "linux":
+        if "TERMUX_VERSION" in os.environ:
+            subprocess.run(["termux-open", link])
+        else:
+            subprocess.run(["xdg-open", link])
+    elif sys.platform == "win32":
+        # `shell=True` is required on Windows!
+        subprocess.run(["start", link], shell=True)
+    elif sys.platform == "darwin":
+        subprocess.run(["open", link])
+    else:
+        raise Exception("unable to open link: unknown platform!")
+
+
+def run():
+    ip = "localhost"
+    port = configs.FLAGS.port
+    addr_str = f"http://{ip}:{port}"
+    if not configs.FLAGS.noserver:
+        # running server is a blocking call, thus if we want to avoid threads, it must be the 
+        # last function we call before ending the program...
+        if configs.FLAGS.gui:
+            open_link(addr_str)
+            print("launching browser to connect to the server...")
+        else:
+            print("not launching browser!")
+        print(f"starting server on {addr_str}")
+        http.server.HTTPServer((ip, port), CustomHTTPHandler).serve_forever()
