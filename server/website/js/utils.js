393fc3a143cacff8eb47ba49a662e5c30253cfad
Daniele Muffato <danixgithub1@gmail.com>
2025-02-05 20:58:24 +0100

    added function to make js request to python server

---
+++ server/website/js/utils.js
@@ -0,0 +1,9 @@
+async function makeRequest(typeField, dataField) {
+    return fetch("", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "type": typeField, "data": dataField }),
+    }).then(res => res.json())
+}
