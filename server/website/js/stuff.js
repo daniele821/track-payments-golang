700b7c95f88f78ca19202195a11be01a9973ee95
Daniele Muffato <danixgithub1@gmail.com>
2025-02-06 07:57:09 +0100

    fixed async method

--- server/website/js/stuff.js
+++ server/website/js/stuff.js
@@ -1,4 +1,18 @@
 import { makeRequest } from "./utils.js"
 
+const sep = ", "
 makeRequest("select-city", {})
-    .then(res => console.log(res))
+    .then(res => res.json())
+    .then(res => console.log("CITY:", res.res.result.join(sep)))
+makeRequest("select-method", {})
+    .then(res => res.json())
+    .then(res => console.log("METHOD:", res.res.result.join(sep)))
+makeRequest("select-shop", {})
+    .then(res => res.json())
+    .then(res => console.log("SHOP:", res.res.result.join(sep)))
+makeRequest("select-category", {})
+    .then(res => res.json())
+    .then(res => console.log("CATEGORY:", res.res.result.join(sep)))
+makeRequest("select-item", {})
+    .then(res => res.json())
+    .then(res => console.log("ITEM:", res.res.result.join(sep)))
--- server/website/js/utils.js
+++ server/website/js/utils.js
@@ -1,9 +1,9 @@
-export async function makeRequest(typeField, dataField) {
+export function makeRequest(typeField, dataField) {
     return fetch("", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ "type": typeField, "data": dataField }),
-    }).then(res => res.json())
+    })
 }
