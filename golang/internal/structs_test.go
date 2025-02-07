a5e6d4894805e83342058f4b35706d806d57840b
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 14:30:53 +0100

    better test

--- golang/internal/structs_test.go
+++ golang/internal/structs_test.go
@@ -18,19 +18,19 @@ func TestAllPayments(t *testing.T) {
 		t.Fatal("there shoul not be any payments!")
 	}
 
-	if err := allPayments.addPayment("Asti", "Coop", "Contante", time.Now()); err != nil {
+	if err := allPayments.addPayment("Asti", "Coop", "Contante", time.Now()); err != nil || len(allPayments.payments) != 1 {
 		t.Fatalf("insertion of new payment failed (%s)!", err)
 	}
 
-	if err := allPayments.addOrder(0, 12, 1245, "Pasta"); err != nil {
+	if err := allPayments.addOrder(0, 12, 1245, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 1 {
 		t.Fatalf("insertion of new order failed (%s)!", err)
 	}
 
-	if err := allPayments.removeOrder(0, "Pasta"); err != nil {
+	if err := allPayments.removeOrder(0, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 0 {
 		t.Fatalf("deletion of order failed (%s)!", err)
 	}
 
-	if err := allPayments.removePayment(0); err != nil {
+	if err := allPayments.removePayment(0); err != nil || len(allPayments.payments) != 0 {
 		t.Fatalf("deletion of payment failed (%s)!", err)
 	}
 }
