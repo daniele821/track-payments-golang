21c880fc1473a5590700a5ee04c8539521aef418
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 23:53:21 +0100

    fixed word

--- golang/internal/utils/algorithms_test.go
+++ golang/internal/utils/algorithms_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestHasDuplicates(t *testing.T) {
 	testSlice := []int{1, 2, 3, 3}
-	if !hasDuplicates(testSlice) {
+	if !HasDuplicates(testSlice) {
 		t.Fatalf("the slice has a duplicate: %v", testSlice)
 	}
 
 	testSlice = []int{1, 2, 3, 4}
-	if hasDuplicates(testSlice) {
+	if HasDuplicates(testSlice) {
 		t.Fatalf("the slice has no duplicate: %v", testSlice)
 	}
 }
