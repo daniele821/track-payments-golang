b1e8ab9958884a169760ae1098faeb0b1e89103d
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 22:42:30 +0100

    saved

--- golang/internal/structs_test.go
+++ golang/internal/structs_test.go
@@ -9,32 +9,32 @@ import (
 func TestConstructors(t *testing.T) {
 	valueSet := valueSet{}
 	allPayments := newAllPayments(valueSet)
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 	if err := allPayments.addCity("Milano"); err != nil {
 		t.Fatalf("insertion on new city failed (%s)!", err)
 	}
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 	if err := allPayments.addShop("Coop"); err != nil {
 		t.Fatalf("insertion on new shop failed (%s)!", err)
 	}
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 	if err := allPayments.addPaymentMethod("Contante"); err != nil {
 		t.Fatalf("insertion on new payment method failed (%s)!", err)
 	}
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 	if err := allPayments.addCategory("Cibo"); err != nil {
 		t.Fatalf("insertion on new category failed (%s)!", err)
 	}
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 	if err := allPayments.addItem("Briosche", "Cibo"); err != nil {
 		t.Fatalf("insertion on new item failed (%s)!", err)
 	}
-	fmt.Println(allPayments.valueSet)
+	fmt.Println(allPayments.ValueSet)
 
 }
 
@@ -47,28 +47,28 @@ func TestAllPayments(t *testing.T) {
 		map[string]string{"Pasta": "Cibo", "Biglietto Treno": "Viaggio"},
 	)
 	allPayments := newAllPayments(valueSet)
-	if len(allPayments.payments) != 0 {
+	if len(allPayments.Payments) != 0 {
 		t.Fatal("there shoul not be any payments!")
 	}
-	fmt.Println(allPayments.payments)
+	fmt.Println(allPayments.Payments)
 
-	if err := allPayments.addPayment("Asti", "Coop", "Contante", time.Now()); err != nil || len(allPayments.payments) != 1 {
+	if err := allPayments.addPayment("Asti", "Coop", "Contante", time.Now()); err != nil || len(allPayments.Payments) != 1 {
 		t.Fatalf("insertion of new payment failed (%s)!", err)
 	}
-	fmt.Println(allPayments.payments)
+	fmt.Println(allPayments.Payments)
 
-	if err := allPayments.addOrder(0, 12, 1245, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 1 {
+	if err := allPayments.addOrder(0, 12, 1245, "Pasta"); err != nil || len(allPayments.Payments[0].Orders) != 1 {
 		t.Fatalf("insertion of new order failed (%s)!", err)
 	}
-	fmt.Println(allPayments.payments)
+	fmt.Println(allPayments.Payments)
 
-	if err := allPayments.removeOrder(0, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 0 {
+	if err := allPayments.removeOrder(0, "Pasta"); err != nil || len(allPayments.Payments[0].Orders) != 0 {
 		t.Fatalf("deletion of order failed (%s)!", err)
 	}
-	fmt.Println(allPayments.payments)
+	fmt.Println(allPayments.Payments)
 
-	if err := allPayments.removePayment(0); err != nil || len(allPayments.payments) != 0 {
+	if err := allPayments.removePayment(0); err != nil || len(allPayments.Payments) != 0 {
 		t.Fatalf("deletion of payment failed (%s)!", err)
 	}
-	fmt.Println(allPayments.payments)
+	fmt.Println(allPayments.Payments)
 }
