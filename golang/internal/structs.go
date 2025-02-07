232dd8b2c44f4309552fc61501b65850c5e99067
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 22:38:29 +0100

    added json conversion

--- golang/internal/structs.go
+++ golang/internal/structs.go
@@ -7,39 +7,39 @@ import (
 )
 
 type valueSet struct {
-	cities         []string
-	shops          []string
-	paymentMethods []string
-	categories     []string
-	items          map[string]string
+	Cities         []string          `json:"cities"`
+	Shops          []string          `json:"shops"`
+	PaymentMethods []string          `json:"paymentMethods"`
+	Categories     []string          `json:"categories"`
+	Items          map[string]string `json:"items"`
 }
 
 type order struct {
-	quantity  int
-	unitPrice int
-	item      string
+	Quantity  int    `json:"quantity"`
+	UnitPrice int    `json:"unitPrice"`
+	Item      string `json:"item"`
 }
 
 type payment struct {
-	city          string
-	shop          string
-	paymentMethod string
-	date          time.Time
-	orders        []order
+	City          string    `json:"city"`
+	Shop          string    `json:"shop"`
+	PaymentMethod string    `json:"paymentMethod"`
+	Date          time.Time `json:"date"`
+	Orders        []order   `json:"orders"`
 }
 
 type allPayments struct {
-	payments []payment
-	valueSet valueSet
+	Payments []payment `json:"payments"`
+	ValueSet valueSet  `json:"valueSet"`
 }
 
 func newValueSet(cities, shops, methods, categories []string, itemCat map[string]string) (valueSet, error) {
 	valueSet := valueSet{
-		cities:         cities,
-		shops:          shops,
-		paymentMethods: methods,
-		categories:     categories,
-		items:          itemCat,
+		Cities:         cities,
+		Shops:          shops,
+		PaymentMethods: methods,
+		Categories:     categories,
+		Items:          itemCat,
 	}
 	for _, category := range itemCat {
 		if !slices.Contains(categories, category) {
@@ -51,105 +51,105 @@ func newValueSet(cities, shops, methods, categories []string, itemCat map[string
 
 func newAllPayments(valueSet valueSet) allPayments {
 	return allPayments{
-		valueSet: valueSet,
+		ValueSet: valueSet,
 	}
 }
 
 func (allPayments *allPayments) addCity(city string) error {
-	if slices.Contains(allPayments.valueSet.cities, city) {
+	if slices.Contains(allPayments.ValueSet.Cities, city) {
 		return errors.New("invalid city: already present in the valueset!")
 	}
-	allPayments.valueSet.cities = append(allPayments.valueSet.cities, city)
+	allPayments.ValueSet.Cities = append(allPayments.ValueSet.Cities, city)
 	return nil
 }
 
 func (allPayments *allPayments) addShop(shop string) error {
-	if slices.Contains(allPayments.valueSet.shops, shop) {
+	if slices.Contains(allPayments.ValueSet.Shops, shop) {
 		return errors.New("invalid shop: already present in the valueset!")
 	}
-	allPayments.valueSet.shops = append(allPayments.valueSet.shops, shop)
+	allPayments.ValueSet.Shops = append(allPayments.ValueSet.Shops, shop)
 	return nil
 }
 
 func (allPayments *allPayments) addPaymentMethod(paymentMethod string) error {
-	if slices.Contains(allPayments.valueSet.paymentMethods, paymentMethod) {
+	if slices.Contains(allPayments.ValueSet.PaymentMethods, paymentMethod) {
 		return errors.New("invalid payment method: already present in the valueset!")
 	}
-	allPayments.valueSet.paymentMethods = append(allPayments.valueSet.paymentMethods, paymentMethod)
+	allPayments.ValueSet.PaymentMethods = append(allPayments.ValueSet.PaymentMethods, paymentMethod)
 	return nil
 }
 
 func (allPayments *allPayments) addCategory(category string) error {
-	if slices.Contains(allPayments.valueSet.categories, category) {
+	if slices.Contains(allPayments.ValueSet.Categories, category) {
 		return errors.New("invalid category: already present in the valueset!")
 	}
-	allPayments.valueSet.categories = append(allPayments.valueSet.categories, category)
+	allPayments.ValueSet.Categories = append(allPayments.ValueSet.Categories, category)
 	return nil
 }
 
 func (allPayments *allPayments) addItem(item, category string) error {
-	if _, ok := allPayments.valueSet.items[item]; ok {
+	if _, ok := allPayments.ValueSet.Items[item]; ok {
 		return errors.New("invalid item: already present in the valueset!")
 	}
-	if !slices.Contains(allPayments.valueSet.categories, category) {
+	if !slices.Contains(allPayments.ValueSet.Categories, category) {
 		return errors.New("invalid category: not in the valueset!")
 	}
-	if allPayments.valueSet.items == nil {
-		allPayments.valueSet.items = map[string]string{}
+	if allPayments.ValueSet.Items == nil {
+		allPayments.ValueSet.Items = map[string]string{}
 	}
-	allPayments.valueSet.items[item] = category
+	allPayments.ValueSet.Items[item] = category
 	return nil
 }
 
 func (allPayments *allPayments) addPayment(city, shop, paymentMethod string, date time.Time) error {
-	if !slices.Contains(allPayments.valueSet.cities, city) {
+	if !slices.Contains(allPayments.ValueSet.Cities, city) {
 		return errors.New("invalid city: not in the valueset!")
 	}
-	if !slices.Contains(allPayments.valueSet.shops, shop) {
+	if !slices.Contains(allPayments.ValueSet.Shops, shop) {
 		return errors.New("invalid shop: not in the valueset!")
 	}
-	if !slices.Contains(allPayments.valueSet.paymentMethods, paymentMethod) {
+	if !slices.Contains(allPayments.ValueSet.PaymentMethods, paymentMethod) {
 		return errors.New("invalid payment payment method: not in the valueset!")
 	}
 	if date.After(time.Now()) {
 		return errors.New("invalid date: date in the future!")
 	}
-	allPayments.payments = append(allPayments.payments, payment{
-		city: city, shop: shop, paymentMethod: paymentMethod, date: date, orders: []order{},
+	allPayments.Payments = append(allPayments.Payments, payment{
+		City: city, Shop: shop, PaymentMethod: paymentMethod, Date: date, Orders: []order{},
 	})
 	return nil
 }
 
 func (allPayments *allPayments) removePayment(paymentIndex int) error {
-	if paymentIndex < 0 || paymentIndex >= len(allPayments.payments) {
+	if paymentIndex < 0 || paymentIndex >= len(allPayments.Payments) {
 		return errors.New("invalid index: out of bound!")
 	}
-	allPayments.payments = slices.Delete(allPayments.payments, paymentIndex, paymentIndex+1)
+	allPayments.Payments = slices.Delete(allPayments.Payments, paymentIndex, paymentIndex+1)
 	return nil
 }
 
 func (allPayments *allPayments) addOrder(paymentIndex, quantity, unitPrice int, item string) error {
-	if _, ok := allPayments.valueSet.items[item]; !ok {
+	if _, ok := allPayments.ValueSet.Items[item]; !ok {
 		return errors.New("invalid item: not in the valueset!")
 	}
-	if paymentIndex < 0 || paymentIndex >= len(allPayments.payments) {
+	if paymentIndex < 0 || paymentIndex >= len(allPayments.Payments) {
 		return errors.New("invalid index: out of bound!")
 	}
-	for _, order := range allPayments.payments[paymentIndex].orders {
-		if order.item == item {
+	for _, order := range allPayments.Payments[paymentIndex].Orders {
+		if order.Item == item {
 			return errors.New("invalid item: duplicate value!")
 		}
 	}
-	allPayments.payments[paymentIndex].orders = append(allPayments.payments[paymentIndex].orders, order{
-		quantity: quantity, unitPrice: unitPrice, item: item,
+	allPayments.Payments[paymentIndex].Orders = append(allPayments.Payments[paymentIndex].Orders, order{
+		Quantity: quantity, UnitPrice: unitPrice, Item: item,
 	})
 	return nil
 }
 
 func (allPayments *allPayments) removeOrder(paymentIndex int, item string) error {
-	if paymentIndex < 0 || paymentIndex >= len(allPayments.payments) {
+	if paymentIndex < 0 || paymentIndex >= len(allPayments.Payments) {
 		return errors.New("invalid index: out of bounds!")
 	}
-	allPayments.payments[paymentIndex].orders = slices.DeleteFunc(allPayments.payments[paymentIndex].orders, func(elem order) bool { return elem.item == item })
+	allPayments.Payments[paymentIndex].Orders = slices.DeleteFunc(allPayments.Payments[paymentIndex].Orders, func(elem order) bool { return elem.Item == item })
 	return nil
 }
