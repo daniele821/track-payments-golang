d818c2ea2a036b8fab29ca1ecd5d7a5bb62a0353
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 14:34:18 +0100

    test has not better output

--- golang/internal/structs_test.go
+++ golang/internal/structs_test.go
@@ -1,6 +1,7 @@
 package payments
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -17,20 +18,25 @@ func TestAllPayments(t *testing.T) {
 	if len(allPayments.payments) != 0 {
 		t.Fatal("there shoul not be any payments!")
 	}
+	fmt.Println(allPayments.payments)
 
 	if err := allPayments.addPayment("Asti", "Coop", "Contante", time.Now()); err != nil || len(allPayments.payments) != 1 {
 		t.Fatalf("insertion of new payment failed (%s)!", err)
 	}
+	fmt.Println(allPayments.payments)
 
 	if err := allPayments.addOrder(0, 12, 1245, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 1 {
 		t.Fatalf("insertion of new order failed (%s)!", err)
 	}
+	fmt.Println(allPayments.payments)
 
 	if err := allPayments.removeOrder(0, "Pasta"); err != nil || len(allPayments.payments[0].orders) != 0 {
 		t.Fatalf("deletion of order failed (%s)!", err)
 	}
+	fmt.Println(allPayments.payments)
 
 	if err := allPayments.removePayment(0); err != nil || len(allPayments.payments) != 0 {
 		t.Fatalf("deletion of payment failed (%s)!", err)
 	}
+	fmt.Println(allPayments.payments)
 }
