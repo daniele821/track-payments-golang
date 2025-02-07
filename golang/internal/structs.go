f678f4addbbd564d801a9a150eeccb3df293d4b8
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 08:48:51 +0100

    added methods to create various structures

--- golang/internal/structs.go
+++ golang/internal/structs.go
@@ -3,6 +3,7 @@ package payments
 import (
 	"errors"
 	"slices"
+	"time"
 )
 
 type valueSet struct {
@@ -14,22 +15,25 @@ type valueSet struct {
 }
 
 type order struct {
-	quantity   int
-	unit_price int
-	item       string
-	category   string
-	valueSet   valueSet
+	quantity  int
+	unitPrice int
+	item      string
 }
 
 type payment struct {
 	city          string
 	shop          string
 	paymentMethod string
+	date          time.Time
 	orders        []order
-	valueSet      valueSet
 }
 
-func newValueSet(cities, shops, methods, categories []string, itemCat map[string]string) (*valueSet, error) {
+type allPayments struct {
+	payments []payment
+	valueSet valueSet
+}
+
+func newValueSet(cities, shops, methods, categories []string, itemCat map[string]string) (valueSet, error) {
 	valueSet := valueSet{
 		cities:        cities,
 		shops:         shops,
@@ -39,8 +43,47 @@ func newValueSet(cities, shops, methods, categories []string, itemCat map[string
 	}
 	for _, category := range itemCat {
 		if !slices.Contains(categories, category) {
-			return nil, errors.New("invalid category!")
+			return valueSet, errors.New("invalid category!")
 		}
 	}
-	return &valueSet, nil
+	return valueSet, nil
+}
+
+func newAllPayments(valueSet valueSet) allPayments {
+	return allPayments{
+		valueSet: valueSet,
+	}
+}
+
+func (payments *allPayments) addPayment(city, shop, paymentMethod string, date time.Time) error {
+	if !slices.Contains(payments.valueSet.cities, city) {
+		return errors.New("invalid city!")
+	}
+	if !slices.Contains(payments.valueSet.shops, shop) {
+		return errors.New("invalid shop!")
+	}
+	if !slices.Contains(payments.valueSet.paymentMethod, paymentMethod) {
+		return errors.New("invalid payment method!")
+	}
+	payments.payments = append(payments.payments, payment{
+		city: city, shop: shop, paymentMethod: paymentMethod, date: date, orders: []order{},
+	})
+	return nil
+}
+
+func (payments *allPayments) addOrder(paymentIndex, quantity, unitPrice int, item string) error {
+	if _, ok := payments.valueSet.itemCat[item]; !ok {
+		return errors.New("invalid item!")
+	}
+	if paymentIndex <= 0 || paymentIndex >= len(payments.payments) {
+		return errors.New("invalid index!")
+	}
+	payments.payments[paymentIndex].orders = append(payments.payments[paymentIndex].orders, order{
+		quantity: quantity, unitPrice: unitPrice, item: item,
+	})
+	return nil
+}
+
+func (payment allPayments) listPayments() []payment {
+	return payment.payments
 }
