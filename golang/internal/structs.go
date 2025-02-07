746cdd8f00b85004bb04242bc8b7dfd5fc2d0d43
Daniele Muffato <danixgithub1@gmail.com>
2025-02-07 13:53:26 +0100

    added methods to remove payments and orders

--- golang/internal/structs.go
+++ golang/internal/structs.go
@@ -55,31 +55,52 @@ func newAllPayments(valueSet valueSet) allPayments {
 	}
 }
 
-func (payments *allPayments) addPayment(city, shop, paymentMethod string, date time.Time) error {
-	if !slices.Contains(payments.valueSet.cities, city) {
+func (allPayments *allPayments) addPayment(city, shop, paymentMethod string, date time.Time) error {
+	if !slices.Contains(allPayments.valueSet.cities, city) {
 		return errors.New("invalid city!")
 	}
-	if !slices.Contains(payments.valueSet.shops, shop) {
+	if !slices.Contains(allPayments.valueSet.shops, shop) {
 		return errors.New("invalid shop!")
 	}
-	if !slices.Contains(payments.valueSet.paymentMethod, paymentMethod) {
+	if !slices.Contains(allPayments.valueSet.paymentMethod, paymentMethod) {
 		return errors.New("invalid payment method!")
 	}
-	payments.payments = append(payments.payments, payment{
+	allPayments.payments = append(allPayments.payments, payment{
 		city: city, shop: shop, paymentMethod: paymentMethod, date: date, orders: []order{},
 	})
 	return nil
 }
 
-func (payments *allPayments) addOrder(paymentIndex, quantity, unitPrice int, item string) error {
-	if _, ok := payments.valueSet.itemCat[item]; !ok {
-		return errors.New("invalid item!")
-	}
-	if paymentIndex <= 0 || paymentIndex >= len(payments.payments) {
+func (allPayments *allPayments) removePayment(paymentIndex int) error {
+	if paymentIndex <= 0 || paymentIndex >= len(allPayments.payments) {
 		return errors.New("invalid index!")
 	}
-	payments.payments[paymentIndex].orders = append(payments.payments[paymentIndex].orders, order{
+	allPayments.payments = slices.Delete(allPayments.payments, paymentIndex, paymentIndex+1)
+	return nil
+}
+
+func (allPayments *allPayments) addOrder(paymentIndex, quantity, unitPrice int, item string) error {
+	if _, ok := allPayments.valueSet.itemCat[item]; !ok {
+		return errors.New("invalid item!")
+	}
+	if paymentIndex <= 0 || paymentIndex >= len(allPayments.payments) {
+		return errors.New("invalid index!")
+	}
+	for _, order := range allPayments.payments[paymentIndex].orders {
+		if order.item == item {
+			return errors.New("duplicate item!")
+		}
+	}
+	allPayments.payments[paymentIndex].orders = append(allPayments.payments[paymentIndex].orders, order{
 		quantity: quantity, unitPrice: unitPrice, item: item,
 	})
+	return nil
+}
+
+func (allPayments *allPayments) removeOrder(paymentIndex int, item string) error {
+	if paymentIndex <= 0 || paymentIndex >= len(allPayments.payments) {
+		return errors.New("invalid index!")
+	}
+	allPayments.payments[paymentIndex].orders = slices.DeleteFunc(allPayments.payments[paymentIndex].orders, func(elem order) bool { return elem.item == item })
 	return nil
 }
