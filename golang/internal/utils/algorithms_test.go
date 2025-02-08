c3cfa388c95f5b0295755b325ab90d24dbb71edf
Daniele Muffato <danixgithub1@gmail.com>
2025-02-08 22:46:15 +0100

    better test

--- golang/internal/utils/algorithms_test.go
+++ golang/internal/utils/algorithms_test.go
@@ -6,19 +6,34 @@ import (
 )
 
 func TestHasDuplicates(t *testing.T) {
+	tests := []struct {
+		name   string
+		args   []any
+		expect bool
+	}{
+		{
+			name:   "HasDuplicatesInt",
+			args:   []any{1, 2, 3, 3},
+			expect: true,
+		},
+		{
+			name:   "HasNoDuplicatesInt",
+			args:   []any{1, 2, 3, 4},
+			expect: false,
+		},
+		{
+			name:   "HasNoDuplicatesString",
+			args:   []any{"", "first", "second"},
+			expect: false,
+		},
+	}
 
-	t.Run("CheckDuplicate", func(t *testing.T) {
-		testSlice := []int{1, 2, 3, 3}
-		if !utils.HasDuplicates(testSlice) {
-			t.Fatalf("the slice has a duplicate: %v", testSlice)
-		}
-	})
-
-	t.Run("CheckNoDuplicates", func(t *testing.T) {
-		testSlice := []int{1, 2, 3, 4}
-		if utils.HasDuplicates(testSlice) {
-			t.Fatalf("the slice has no duplicate: %v", testSlice)
-		}
-	})
-
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := utils.HasDuplicates(test.args)
+			if actual != test.expect {
+				t.Fatalf("%v -> expected: %t, actual: %t", test.args, test.expect, actual)
+			}
+		})
+	}
 }
