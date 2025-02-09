9a8b779294372a03785f1ccf7252851f0c9fbe33
Daniele Muffato <danixgithub1@gmail.com>
2025-02-09 17:41:14 +0100

    removed useless tests

--- golang/internal/server/structures/payments_test.go
+++ golang/internal/server/structures/payments_test.go
@@ -4,86 +4,7 @@ import (
 	"payment/internal/server/structures"
 	"reflect"
 	"testing"
-	"time"
 )
-
-func TestConstructors(t *testing.T) {
-	valueSet := structures.ValueSet{}
-	allPayments := structures.NewAllPayments(valueSet)
-
-	t.Run("InsertCity", func(t *testing.T) {
-		if err := allPayments.AddCity("Milano"); err != nil {
-			t.Fatalf("insertion on new city failed (%s)!", err)
-		}
-	})
-
-	t.Run("InsertShop", func(t *testing.T) {
-		if err := allPayments.AddShop("Coop"); err != nil {
-			t.Fatalf("insertion on new shop failed (%s)!", err)
-		}
-	})
-
-	t.Run("InsertPaymentMethod", func(t *testing.T) {
-		if err := allPayments.AddPaymentMethod("Contante"); err != nil {
-			t.Fatalf("insertion on new payment method failed (%s)!", err)
-		}
-	})
-
-	t.Run("InsertCategory", func(t *testing.T) {
-		if err := allPayments.AddCategory("Cibo"); err != nil {
-			t.Fatalf("insertion on new category failed (%s)!", err)
-		}
-	})
-
-	t.Run("InsertItem", func(t *testing.T) {
-		if err := allPayments.AddItem("Briosche", "Cibo"); err != nil {
-			t.Fatalf("insertion on new item failed (%s)!", err)
-		}
-	})
-
-}
-
-func TestAllPayments(t *testing.T) {
-	valueSet, _ := structures.NewValueSet(
-		[]string{"Asti", "Cesena", "Milano", "Roma"},
-		[]string{"Coop", "Paninaro", "Conad", "Ristorante Indiano"},
-		[]string{"Contante", "Postepay", "San Paolo"},
-		[]string{"Cibo", "Viaggio"},
-		map[string]string{"Pasta": "Cibo", "Biglietto Treno": "Viaggio"},
-	)
-	allPayments := structures.NewAllPayments(valueSet)
-
-	t.Run("AssertNoPayments", func(t *testing.T) {
-		if len(allPayments.Payments) != 0 {
-			t.Fatal("there shoul not be any payments!")
-		}
-	})
-
-	t.Run("InsertPayment", func(t *testing.T) {
-		if err := allPayments.AddPayment("Asti", "Coop", "Contante", time.Now()); err != nil || len(allPayments.Payments) != 1 {
-			t.Fatalf("insertion of new payment failed (%s)!", err)
-		}
-	})
-
-	t.Run("InsertOrder", func(t *testing.T) {
-		if err := allPayments.AddOrder(0, 12, 1245, "Pasta"); err != nil || len(allPayments.Payments[0].Orders) != 1 {
-			t.Fatalf("insertion of new order failed (%s)!", err)
-		}
-	})
-
-	t.Run("RemoveOrder", func(t *testing.T) {
-		if err := allPayments.RemoveOrder(0, "Pasta"); err != nil || len(allPayments.Payments[0].Orders) != 0 {
-			t.Fatalf("deletion of order failed (%s)!", err)
-		}
-	})
-
-	t.Run("RemovePayment", func(t *testing.T) {
-		if err := allPayments.RemovePayment(0); err != nil || len(allPayments.Payments) != 0 {
-			t.Fatalf("deletion of payment failed (%s)!", err)
-		}
-	})
-
-}
 
 func TestJson(t *testing.T) {
 	valueSet, _ := structures.NewValueSet(
@@ -94,8 +15,8 @@ func TestJson(t *testing.T) {
 		map[string]string{"Pasta": "Cibo", "Biglietto Treno": "Viaggio"},
 	)
 	allPayments := structures.NewAllPayments(valueSet)
-	allPayments.AddPayment("Asti", "Coop", "Contante", time.Now())
-	allPayments.AddOrder(0, 12, 1245, "Pasta")
+	allPayments.AddPayment("Asti", "Coop", "Contante", "2025-12-03 12:34")
+	allPayments.AddOrder(0, 12, "2025-12-03 12:34", "Pasta")
 
 	paymentsJson, err := allPayments.GenerateJson(false)
 	t.Run("ConvertToJson", func(t *testing.T) {
