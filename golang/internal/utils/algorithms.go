baa11b1431ed9f326d6aa91e93a27e04fe6ca36f
Daniele Muffato <danixgithub1@gmail.com>
2025-02-09 19:03:52 +0100

    better duplicate checker function

--- golang/internal/utils/algorithms.go
+++ golang/internal/utils/algorithms.go
@@ -1,12 +1,19 @@
 package utils
 
-func HasDuplicates[T comparable](slice []T) bool {
-	seen := map[T]bool{}
-	for _, item := range slice {
-		if _, ok := seen[item]; ok {
+func HasDuplicatesMapped[S ~[]E, E any, M comparable](slice S, mapFunc func(index int, elem E) M) bool {
+	seen := map[M]bool{}
+	for index, item := range slice {
+		mappedItem := mapFunc(index, item)
+		if _, ok := seen[mappedItem]; ok {
 			return true
 		}
-		seen[item] = true
+		seen[mappedItem] = true
 	}
 	return false
+}
+
+func HasDuplicates[T comparable](slice []T) bool {
+	return HasDuplicatesMapped(slice, func(index int, item T) T {
+		return item
+	})
 }
