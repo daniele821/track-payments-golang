ce07dd2e9427a0a66fd619f68f7f032cdcc26d25
Daniele Muffato <danixgithub1@gmail.com>
2025-02-09 19:27:48 +0100

    added btree

--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,5 @@
 module payment
 
 go 1.23.5
+
+require github.com/google/btree v1.1.3 // indirect
--- golang/internal/server/structures/payments.go
+++ golang/internal/server/structures/payments.go
@@ -1,23 +1,25 @@
 package structures
 
+import "github.com/google/btree"
+
 type ValueSet struct {
-	cities         []*string
-	shops          []*string
-	paymentMethods []*string
-	items          []*string
+	cities         []string
+	shops          []string
+	paymentMethods []string
+	items          []string
 }
 
 type Order struct {
 	quantity  int
 	unitPrice int
-	item      *string
+	item      btree.BTreeG[string]
 }
 
 type Payment struct {
-	city          *string
-	shop          *string
-	paymentMethod *string
-	date          string
+	city          string
+	shop          string
+	paymentMethod string
+	date          btree.BTreeG[string]
 	description   string
 	orders        []Order
 }
