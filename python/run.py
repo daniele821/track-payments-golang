20329c42a0d97145a219d39c4fc77b008234c776
Daniele Muffato <danixgithub1@gmail.com>
2025-02-09 20:58:21 +0100

    removed db + python

--- python/run.py
+++
@@ -1,6 +0,0 @@
-#!/bin/python3
-
-import server
-import configs
-
-server.run("payments", "localhost", configs.FLAGS.port, configs.FLAGS.noserver, configs.FLAGS.gui)
--- python/website/js/utils.js
+++
@@ -1,9 +0,0 @@
-export function makeRequest(typeField, dataField) {
-    return fetch("", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "type": typeField, "data": dataField }),
-    })
-}
