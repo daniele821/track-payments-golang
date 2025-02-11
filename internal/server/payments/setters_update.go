45508f7f023159f80ab3a8494fd67b92ff8d4dea
Daniele Muffato <danixgithub1@gmail.com>
2025-02-11 19:10:38 +0100

    added update methods

--- internal/server/payments/setters_update.go
+++ internal/server/payments/setters_update.go
@@ -1,1 +1,41 @@
 package payments
+
+func (allPayments *AllPayments) UpdatePayment(date string, city, shop, paymentMethod, description *string) error {
+	if err := allPayments.checks(&date, city, shop, paymentMethod, nil); err != nil {
+		return err
+	}
+	payment, err := allPayments.Payment(date)
+	if err != nil {
+		return err
+	}
+	if city != nil {
+		payment.city = *city
+	}
+	if shop != nil {
+		payment.shop = *shop
+	}
+	if paymentMethod != nil {
+		payment.paymentMethod = *paymentMethod
+	}
+	if description != nil {
+		payment.description = *description
+	}
+	return nil
+}
+
+func (allPayments *AllPayments) UpdateOrder(date, item string, quantity, unitPrice *uint) error {
+	if err := allPayments.checks(&date, nil, nil, nil, &item); err != nil {
+		return err
+	}
+	order, err := allPayments.Order(date, item)
+	if err != nil {
+		return err
+	}
+	if quantity != nil {
+		order.quantity = *quantity
+	}
+	if unitPrice != nil {
+		order.unitPrice = *unitPrice
+	}
+	return nil
+}
