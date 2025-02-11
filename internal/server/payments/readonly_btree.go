4740d87e0d5bc25cd89bb06ea68eeba4138ab0fe
Daniele Muffato <danixgithub1@gmail.com>
2025-02-11 18:45:22 +0100

    added single func to handle traversal

--- internal/server/payments/readonly_btree.go
+++ internal/server/payments/readonly_btree.go
@@ -17,3 +17,33 @@ func (readOnlyBTree *ReadOnlyBTree[T]) Min() (T, bool) {
 func (readOnlyBTree *ReadOnlyBTree[T]) Max() (T, bool) {
 	return readOnlyBTree.btree.Max()
 }
+
+func (readOnlyBTree *ReadOnlyBTree[T]) Ascend(iterator btree.ItemIteratorG[T], first, last *T) {
+	switch {
+	case first == nil && last == nil:
+		readOnlyBTree.btree.Ascend(iterator)
+	case first == nil && last != nil:
+		readOnlyBTree.btree.AscendLessThan(*last, iterator)
+	case first != nil && last == nil:
+		readOnlyBTree.btree.AscendGreaterOrEqual(*first, iterator)
+	case first != nil && last != nil:
+		readOnlyBTree.btree.AscendRange(*first, *last, iterator)
+	default:
+		panic("UNREACHABLE CODE: all possible cases should have already been covered!")
+	}
+}
+
+func (readOnlyBTree *ReadOnlyBTree[T]) Descend(iterator btree.ItemIteratorG[T], first, last *T) {
+	switch {
+	case first == nil && last == nil:
+		readOnlyBTree.btree.Descend(iterator)
+	case first == nil && last != nil:
+		readOnlyBTree.btree.DescendGreaterThan(*last, iterator)
+	case first != nil && last == nil:
+		readOnlyBTree.btree.DescendLessOrEqual(*first, iterator)
+	case first != nil && last != nil:
+		readOnlyBTree.btree.DescendRange(*first, *last, iterator)
+	default:
+		panic("UNREACHABLE CODE: all possible cases should have already been covered!")
+	}
+}
