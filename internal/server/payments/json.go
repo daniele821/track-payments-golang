b9b8c1ad4cd6b400a0ce48725e6bacc52b0b7f79
Daniele Muffato <danixgithub1@gmail.com>
2025-02-12 16:00:07 +0100

    added json data structures

--- internal/server/payments/json.go
+++ internal/server/payments/json.go
@@ -1,5 +1,40 @@
 package payments
 
+type valueSetJson struct {
+	Cities         []string `json:"cities"`
+	Shops          []string `json:"shops"`
+	PaymentMethods []string `json:"paymentMethods"`
+	Items          []string `json:"items"`
+}
+
+type orderJson struct {
+	Quantity  uint   `json:"quantity"`
+	UnitPrice uint   `json:"unitPrice"`
+	Item      string `json:"item"`
+}
+
+type paymentJson struct {
+	City          string  `json:"city"`
+	Shop          string  `json:"shop"`
+	PaymentMethod string  `json:"paymentMethod"`
+	Date          string  `json:"date"`
+	Description   string  `json:"description"`
+	Orders        []Order `json:"orders"`
+}
+
+type allPaymentsJson struct {
+	Payments []Payment `json:"payments"`
+	ValueSet ValueSet  `json:"valueSet"`
+}
+
+func convertToJsonData(input AllPayments) (output allPaymentsJson) {
+	return output
+}
+
+func convertFromJsonData(input allPaymentsJson) (output AllPayments, err error) {
+	return output, nil
+}
+
 func NewAllPaymentsFromJson(allPaymentsJson string) (AllPayments, error) {
 	panic("TODO: convert map to allPayments")
 }
