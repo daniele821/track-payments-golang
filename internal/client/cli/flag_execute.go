25c72a2c3a7d60ecdd04b326eca3f43a84a7dff0
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 13:57:43 +0100

    insertion operations

--- internal/client/cli/flag_execute.go
+++ internal/client/cli/flag_execute.go
@@ -1,6 +1,21 @@
 package cli
 
-import "payment/internal/server/payments"
+import (
+	"fmt"
+	"payment/internal/server/payments"
+)
+
+func insert(typeData, data string, addFunc func(string) error) {
+	if data == "" {
+		fmt.Printf("no %s was passed\n", typeData)
+	} else {
+		if err := addFunc(data); err != nil {
+			fmt.Printf("%s insertion failed: %s\n", typeData, err)
+		}
+		fmt.Printf("successfully added %s %s\n", typeData, data)
+	}
+	return
+}
 
 func (f flags) execute(allPayments payments.AllPayments) {
 	insertAct := *f.insertAction
@@ -10,9 +25,13 @@ func (f flags) execute(allPayments payments.AllPayments) {
 	if insertAct != "" && listAct == "" && updateAct == "" && deleteAct == "" {
 		switch insertAct {
 		case "city":
+			insert("city", *f.cityData, func(s string) error { return allPayments.AddCities(s) })
 		case "shop":
+			insert("shop", *f.shopData, func(s string) error { return allPayments.AddShops(s) })
 		case "method":
+			insert("method", *f.methodData, func(s string) error { return allPayments.AddPaymentMethods(s) })
 		case "item":
+			insert("item", *f.itemData, func(s string) error { return allPayments.AddItems(s) })
 		case "payment":
 		case "order":
 		}
