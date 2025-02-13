ca8dc8ee41362b6252cdec5806e8a98d7771212d
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 20:40:40 +0100

    added some more parsing

--- internal/client/cli_manual/parser.go
+++ internal/client/cli_manual/parser.go
@@ -5,12 +5,7 @@ import (
 	"fmt"
 	"os"
 	"payment/internal/server/payments"
-	"strings"
 )
-
-func errMsgForWrongArgs(args []string, wrongAt int) error {
-	return errors.New(fmt.Sprintf("invalid args for %s: %s", strings.Join(args[:wrongAt], " "), strings.Join(args[wrongAt:], " ")))
-}
 
 func parseParamsAndRun(allPayments payments.AllPayments) error {
 	return parseAndRun(allPayments, os.Args[1:])
@@ -20,33 +15,55 @@ func parseAndRun(allPayments payments.AllPayments, args []string) error {
 	if len(args) == 0 {
 		return nil
 	}
-	arg0 := args[0]
-	arg1 := args[1]
 	switch {
-	case matchEveryLenght(arg0, "insert"):
+	case matchEveryLenght(args[0], "insert"):
 		switch {
-		}
-	case matchEveryLenght(arg0, "list"):
-		switch {
-		case matchEveryLenghtFromAnyWords(arg1, []string{"city", "cities"}):
-		case matchEveryLenghtFromAnyWords(arg1, []string{"shops"}):
-		case matchEveryLenghtFromAnyWords(arg1, []string{"methods"}):
-		case matchEveryLenghtFromAnyWords(arg1, []string{"items"}):
-		case matchEveryLenghtFromAnyWords(arg1, []string{"payments"}):
-		case matchEveryLenghtFromAnyWords(arg1, []string{"orders"}):
+		case len(args) <= 1:
+			return errors.New("missing arg for insert")
+		case matchEveryLenghtFromAnyWords(args[1], []string{"city", "cities"}):
+		case matchEveryLenght(args[1], "shops"):
+		case matchEveryLenght(args[1], "methods"):
+		case matchEveryLenght(args[1], "items"):
+		case matchEveryLenght(args[1], "payments"):
+		case matchEveryLenght(args[1], "orders"):
 		default:
-			return errMsgForWrongArgs(args, 1)
+			return errors.New(fmt.Sprintf("invalid arg for insert: %s", args[1]))
 		}
-	case matchEveryLenght(arg0, "update"):
+	case matchEveryLenght(args[0], "list"):
 		switch {
+		case len(args) <= 1:
+			return errors.New("missing arg for list")
+		case matchEveryLenghtFromAnyWords(args[1], []string{"city", "cities"}):
+		case matchEveryLenght(args[1], "shops"):
+		case matchEveryLenght(args[1], "methods"):
+		case matchEveryLenght(args[1], "items"):
+		case matchEveryLenght(args[1], "payments"):
+		case matchEveryLenght(args[1], "orders"):
+		default:
+			return errors.New(fmt.Sprintf("invalid arg for list: %s", args[1]))
 		}
-	case matchEveryLenght(arg0, "delete"):
+	case matchEveryLenght(args[0], "update"):
 		switch {
+		case len(args) <= 1:
+			return errors.New("missing arg for update")
+		case matchEveryLenght(args[1], "payments"):
+		case matchEveryLenght(args[1], "orders"):
+		default:
+			return errors.New(fmt.Sprintf("invalid arg for update: %s", args[1]))
 		}
-	case matchEveryLenght(arg0, "help"):
+	case matchEveryLenght(args[0], "delete"):
+		switch {
+		case len(args) <= 1:
+			return errors.New("missing arg for delete")
+		case matchEveryLenght(args[1], "payments"):
+		case matchEveryLenght(args[1], "orders"):
+		default:
+			return errors.New(fmt.Sprintf("invalid arg for delete: %s", args[1]))
+		}
+	case matchEveryLenght(args[0], "help"):
 		helpMsg()
 	default:
-		return errMsgForWrongArgs(args, 0)
+		return errors.New(fmt.Sprintf("invalid arg: %s", args[0]))
 	}
 	return nil
 }
