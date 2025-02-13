a34b87713adeb83e89d566713f20dec198986296
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 14:39:15 +0100

    added bash script to easily run compiler if exe not exists, otherwise just run executable

---
+++ payments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+
+GOMAIN_PATH="${SCRIPT_DIR}/cmd/payments/main.go"
+BIN_PATH="${SCRIPT_DIR}/data/payments"
+
+[[ -v BUILD ]] && rm "$BIN_PATH"
+
+[[ -f "$BIN_PATH" ]] || go build -o "$BIN_PATH" "$GOMAIN_PATH"
+
+"$BIN_PATH" "$@"
