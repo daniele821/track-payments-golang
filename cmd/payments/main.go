e99f57aa243d85d7015bf27bc60d2f3946fe676c
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 14:25:42 +0100

    written runner func

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"payment/internal/client/cli"
 )
 
 func main() {
-	cli.Run()
+	if err := cli.Run(); err != nil {
+		fmt.Printf("failed: %s", err)
+	}
 }
--- internal/client/cli/runner.go
+++ internal/client/cli/runner.go
@@ -1,7 +1,57 @@
 package cli
 
-import "payment/internal/server/payments"
+import (
+	"os"
+	"path/filepath"
+	"payment/internal/server/payments"
+)
 
-func Run() {
-	addFlags().execute(payments.NewAllPayments())
+func Run() error {
+	// load json file
+	exePath, err := os.Executable()
+	if err != nil {
+		return err
+	}
+	exePath, err = filepath.EvalSymlinks(exePath)
+	if err != nil {
+		return err
+	}
+	jsonDir := filepath.Dir(exePath)
+	jsonPath := filepath.Join(jsonDir, "payments.json")
+	jsonDataByte, err := os.ReadFile(jsonPath)
+	if err != nil {
+		fileCreated, err := os.Create(jsonPath)
+		if err != nil {
+			return err
+		}
+		defer fileCreated.Close()
+		if _, err := fileCreated.WriteString("{}"); err != nil {
+			return err
+		}
+		jsonDataByte, err = os.ReadFile(jsonPath)
+		if err != nil {
+			return err
+		}
+	}
+	JsonData := string(jsonDataByte)
+
+	// load all payments from json file
+	allPayments, err := payments.NewAllPaymentsFromJson(JsonData)
+	if err != nil {
+		return err
+	}
+
+	// execute program
+	addFlags().execute(allPayments)
+
+	// dump all payments to json file
+	jsonData, err := allPayments.DumpJson(true)
+	if err != nil {
+		return err
+	}
+	if err := os.WriteFile(jsonPath, []byte(jsonData), 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
