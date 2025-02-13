6e948c589f8bb3145b62ebf339253f70d5f1cbf1
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 07:48:22 +0100

    added unix-like flag parser

--- internal/client/cli/flag_parser.go
+++ internal/client/cli/flag_parser.go
@@ -1,5 +1,32 @@
 package cli
 
+import (
+	"strings"
+)
+
 type Flags struct {
-	flags map[string]string
+	flagArgs  map[string][]string
+	flagOrder []string
+}
+
+func ParseFlags(args []string) Flags {
+	flagArgs := map[string][]string{}
+	flagOrder := []string{}
+
+	tmpFlag := ""
+	tmpArg := []string{}
+
+	for _, arg := range append(args, "--") {
+		if strings.HasPrefix(arg, "-") {
+			flagOrder = append(flagOrder, arg)
+
+			flagArgs[tmpFlag] = tmpArg
+			tmpFlag = arg
+			tmpArg = []string{}
+		} else {
+			tmpArg = append(tmpArg, arg)
+		}
+	}
+
+	return Flags{flagArgs: flagArgs, flagOrder: flagOrder[:len(flagOrder)-1]}
 }
---
+++ internal/client/cli/flag_test.go
@@ -0,0 +1,18 @@
+package cli
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFlagParse(t *testing.T) {
+	args := []string{"word1", "-abc", "word2", "-abde", "--flag1", "word3", "word4"}
+	expectedFlags := Flags{
+		flagArgs:  map[string][]string{"": {"word1"}, "-abc": {"word2"}, "-abde": {}, "--flag1": {"word3", "word4"}},
+		flagOrder: []string{"-abc", "-abde", "--flag1"},
+	}
+	actualFlags := ParseFlags(args)
+	if !reflect.DeepEqual(expectedFlags, actualFlags) {
+		t.Fatalf("flag parsing was wrong: \nexpected: %s \nactual: %s", expectedFlags, actualFlags)
+	}
+}
