4dab88a78658ced02ff988e935db9b2b188a46ac
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 12:39:00 +0100

    use golang flag library

--- internal/client/cli/flags/flag_tostring.go
+++
@@ -1,7 +0,0 @@
-package flags
-
-import "fmt"
-
-func (f FlagParsed) String() string {
-	return fmt.Sprintf("flagArgs: %v", f.flagArgs)
-}
--- internal/client/cli/runner.go
+++ internal/client/cli/runner.go
@@ -1,12 +1,4 @@
 package cli
 
-import (
-	"fmt"
-	"os"
-	"payment/internal/client/cli/flags"
-)
-
 func Run() {
-	flags, _ := flags.NewFlagParsed(os.Args[1:])
-	fmt.Println(flags)
 }
