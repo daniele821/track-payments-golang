934a68615d36d3847360ef0cd9fcfb8f893f7884
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 21:21:40 +0100

    added box chars

---
+++ internal/client/cli_manual/run_visualizer.go
@@ -0,0 +1,13 @@
+package cli_manual
+
+const BoxVert string = "│"
+const BoxHoriz string = "─"
+const BoxLeftTop string = "┘"
+const BoxRightTop string = "└"
+const BoxLeftDown string = "┐"
+const BoxRightDown string = "┌"
+const BoxVertLeft string = "┤"
+const BoxVertRight string = "├"
+const BoxHorizTop string = "┴"
+const BoxHorizDown string = "┬"
+const BoxCross string = "┼"
