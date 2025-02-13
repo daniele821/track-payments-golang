6f3b7913121a124f71ebff9d89169353a19b38a8
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 20:41:35 +0100

    updated help msg

--- internal/client/cli_manual/help.go
+++ internal/client/cli_manual/help.go
@@ -6,12 +6,12 @@ func helpMsg() {
 	fmt.Print(`Usage of ./payments
 
 Operations: 
-    i[nsert] c[ity] CITY ...
-    i[nsert] s[hop] SHOP ...
-    i[nsert] m[ethod] METHOD ...
-    i[nsert] i[tem] ITEM ...
-    i[nsert] p[ayment] DATE CITY SHOP METHOD [DESCRIPTION] [@ ITEM1 QUANTITY1 PRICE1 @ ...]
-    i[nsert] o[rder] DATE ITEM QUANTITY PRICE [@ ITEM2 QUANTITY2 PRICE2 @ ...] 
+    i[nsert] c[ity|ities] CITY ...
+    i[nsert] s[hops] SHOP ...
+    i[nsert] m[ethods] METHOD ...
+    i[nsert] i[tems] ITEM ...
+    i[nsert] p[ayments] DATE CITY SHOP METHOD [DESCRIPTION] [@ ITEM1 QUANTITY1 PRICE1 @ ...]
+    i[nsert] o[rders] DATE ITEM QUANTITY PRICE [@ ITEM2 QUANTITY2 PRICE2 @ ...] 
 
     l[ist] c[ity|ities]
     l[ist] s[hops]
@@ -20,11 +20,11 @@ Operations:
     l[ist] p[ayments]
     l[ist] o[rders]
 
-    u[pdate] p[ayment] DATE [CITY] [SHOP] [METHOD] [DESCRIPTION] [@ DATE2 [CITY2] [SHOP2] [METHOD2] [DESCRIPTION2] @ ...]
-    u[pdate] o[rder] DATE ITEM [QUANTITY] [PRICE] [@ DATE2 ITEM2 [QUANTITY2] [PRICE2] @ ...]
+    u[pdate] p[ayments] DATE [CITY] [SHOP] [METHOD] [DESCRIPTION] [@ DATE2 [CITY2] [SHOP2] [METHOD2] [DESCRIPTION2] @ ...]
+    u[pdate] o[rders] DATE ITEM [QUANTITY] [PRICE] [@ DATE2 ITEM2 [QUANTITY2] [PRICE2] @ ...]
 
-    d[elete] p[ayment] DATE [DATE2 ...]
-    d[elete] o[rder] DATE ITEM [@ [DATE2] ITEM2 @ ...]
+    d[elete] p[ayments] DATE [DATE2 ...]
+    d[elete] o[rders] DATE ITEM [@ [DATE2] ITEM2 @ ...]
     
     h[elp]
 `)
