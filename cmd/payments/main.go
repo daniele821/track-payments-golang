3feee7a6dd11d5ca3f33c3145cfdaa3cd4feae85
Daniele Muffato <danixgithub1@gmail.com>
2025-02-13 21:23:40 +0100

    changed back to newer client

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"payment/internal/client/cli_goflag"
+	"payment/internal/client/cli_manual"
 )
 
 const jsonPath string = "payments.json"
 
 func main() {
-	if err := cli_goflag.Run(jsonPath); err != nil {
+	if err := cli_manual.Run(jsonPath); err != nil {
 		fmt.Printf("execution failed: %s\n", err)
 	}
 }
