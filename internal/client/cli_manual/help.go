c024096ee6cdc9bd8d145018f25c1c4b4036aa01
Daniele Muffato <danixgithub1@gmail.com>
2025-02-14 15:06:51 +0100

    updated help

--- internal/client/cli_manual/help.go
+++ internal/client/cli_manual/help.go
@@ -10,9 +10,9 @@ Operations:
     i[nsert] s[hops] SHOP [...]
     i[nsert] m[ethods] METHOD [...]
     i[nsert] i[tems] ITEM [...]
-    i[nsert] p[ayments] [[DATE] TIME] CITY SHOP METHOD DESCRIPTION|* [@ ...]
-    i[nsert] o[rders] [[DATE] TIME] ITEM QUANTITY PRICE [@ ...]
-    i[nsert] d[etails] [[DATE] TIME] CITY SHOP METHOD DESCRIPTION|* [@ ITEM QUANTITY PRICE] [@ ...]
+    i[nsert] p[ayments] DATE|* TIME|* CITY SHOP METHOD DESCRIPTION|* [@ ...]
+    i[nsert] o[rders] DATE|* TIME|* ITEM QUANTITY PRICE [@ ...]
+    i[nsert] d[etails] DATE|* TIME|* CITY SHOP METHOD DESCRIPTION|* [@ ITEM QUANTITY PRICE] [@ ...]
 
     l[ist]|v[isualize] c[ity|ities]
     l[ist]|v[isualize] s[hops]
@@ -21,19 +21,13 @@ Operations:
     l[ist]|v[isualize] v[alues]
     l[ist]|v[isualize] p[ayments]
 
-    u[pdate] p[ayments] DATE TIME CITY SHOP METHOD DESCRIPTION|* [@ ...]
-    u[pdate] o[rders] DATE TIME ITEM QUANTITY PRICE [@ ...]
-    i[nsert] d[etails] DATE TIME CITY SHOP METHOD DESCRIPTION|* [@ ITEM QUANTITY PRICE] [@ ...]
+    u[pdate] p[ayments] DATE|* TIME|* CITY|* SHOP|* METHOD|* DESCRIPTION|* [@ ...]
+    u[pdate] o[rders] DATE|* TIME|* ITEM QUANTITY|* PRICE|* [@ ...]
+    u[pdate] d[etails] DATE|* TIME|* CITY|* SHOP|* METHOD|* DESCRIPTION|* [@ ITEM QUANTITY PRICE] [@ ...]
 
-    d[elete] p[ayments] [[DATE] TIME] [@...]
-    d[elete] o[rders] [[DATE] TIME] ITEM [@ ...]
+    d[elete] p[ayments] DATE|* TIME|* [@...]
+    d[elete] o[rders] DATE|* TIME|* ITEM [@ ...]
     
     h[elp]
 `)
 }
-
-/*
-TODO:
-    - think about allowing passing args as name=value instead of relying on just the order
-    - rethink update operations: OPTIONAL params do NOT work if i rely on order. How to do it? have a nil value? (maybe - or -- ?)
-*/
