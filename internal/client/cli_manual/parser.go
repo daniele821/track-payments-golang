25ea2e42064f74744642ab92368e20770245efa5
Daniele Muffato <danixgithub1@gmail.com>
2025-02-14 16:05:02 +0100

    error when fail, instead of just printint it out

--- internal/client/cli_manual/parser.go
+++ internal/client/cli_manual/parser.go
@@ -21,19 +21,19 @@ func parseAndRun(allPayments payments.AllPayments, args []string) error {
 		case len(args) <= 1:
 			return errors.New("missing arg for insert")
 		case matchEveryLenghtFromAnyWords(args[1], []string{"city", "cities"}):
-			insertGeneric("cities", args[2:], allPayments.AddCities)
+			return insertGeneric("cities", args[2:], allPayments.AddCities)
 		case matchEveryLenght(args[1], "shops"):
-			insertGeneric("shops", args[2:], allPayments.AddShops)
+			return insertGeneric("shops", args[2:], allPayments.AddShops)
 		case matchEveryLenght(args[1], "methods"):
-			insertGeneric("methods", args[2:], allPayments.AddPaymentMethods)
+			return insertGeneric("methods", args[2:], allPayments.AddPaymentMethods)
 		case matchEveryLenght(args[1], "items"):
-			insertGeneric("items", args[2:], allPayments.AddItems)
+			return insertGeneric("items", args[2:], allPayments.AddItems)
 		case matchEveryLenght(args[1], "payments"):
-			insertPayments(allPayments, args[2:])
+			return insertPayments(allPayments, args[2:])
 		case matchEveryLenght(args[1], "orders"):
-			insertOrders(allPayments, args[2:])
+			return insertOrders(allPayments, args[2:])
 		case matchEveryLenght(args[1], "details"):
-			insertDetails(allPayments, args[2:])
+			return insertDetails(allPayments, args[2:])
 		default:
 			return errors.New(fmt.Sprintf("invalid arg for insert: %s", args[1]))
 		}
--- internal/client/cli_manual/run_insertions.go
+++ internal/client/cli_manual/run_insertions.go
@@ -1,31 +1,31 @@
 package cli_manual
 
 import (
+	"errors"
 	"fmt"
 	"payment/internal/server/payments"
 	"strconv"
 	"strings"
 )
 
-func insertGeneric(dataType string, data []string, insertFunc func(data ...string) error) {
+func insertGeneric(dataType string, data []string, insertFunc func(data ...string) error) error {
 	if len(data) == 0 {
-		fmt.Printf("no %s passed\n", dataType)
-		return
+		return errors.New(fmt.Sprintf("no %s passed\n", dataType))
 	}
 	if err := insertFunc(data...); err != nil {
 		fmt.Printf("%s insertion failed: %s\n", dataType, err)
 	} else {
 		fmt.Printf("successfully inserted %s (%s)\n", dataType, strings.Join(data, ", "))
 	}
+	return nil
 }
 
-func insertPayments(allPayments payments.AllPayments, data []string) {
+func insertPayments(allPayments payments.AllPayments, data []string) error {
 	okMsg := []string{}
 	dateStr, timeStr := getDateAndTime()
 	for index, splittedData := range splitter(data) {
 		if len(splittedData) != 6 {
-			fmt.Printf("invalid amount of parameters to insert the %dth payment (%s)\n", index, strings.Join(splittedData, ", "))
-			return
+			return errors.New(fmt.Sprintf("invalid amount of parameters to insert the %dth payment (%s)\n", index, strings.Join(splittedData, ", ")))
 		}
 		dateStr = fillDataIfEmpty(splittedData[0], dateStr)
 		timeStr = fillDataIfEmpty(splittedData[1], timeStr)
@@ -35,22 +35,21 @@ func insertPayments(allPayments payments.AllPayments, data []string) {
 		description := fillDataIfEmpty(splittedData[5], "")
 		dateFinal := dateStr + " " + timeStr
 		if err := allPayments.AddPayment(city, shop, method, dateFinal, description); err != nil {
-			fmt.Printf("payment (%d) insertion failed: %s\n", index, err)
-			return
+			return errors.New(fmt.Sprintf("payment (%d) insertion failed: %s\n", index, err))
 		} else {
 			okMsg = append(okMsg, fmt.Sprintf("successfully inserted payment (%s, %s, %s, %s, %s)\n", dateFinal, city, shop, method, description))
 		}
 	}
 	fmt.Println(strings.Join(okMsg, ""))
+	return nil
 }
 
-func insertOrders(allPayments payments.AllPayments, data []string) {
+func insertOrders(allPayments payments.AllPayments, data []string) error {
 	okMsg := []string{}
 	dateStr, timeStr := getDateAndTime()
 	for index, splittedData := range splitter(data) {
 		if len(splittedData) != 5 {
-			fmt.Printf("invalid amount of parameters to insert the %dth order (%s)\n", index, strings.Join(splittedData, ", "))
-			return
+			return errors.New(fmt.Sprintf("invalid amount of parameters to insert the %dth order (%s)\n", index, strings.Join(splittedData, ", ")))
 		}
 		dateStr = fillDataIfEmpty(splittedData[0], dateStr)
 		timeStr = fillDataIfEmpty(splittedData[1], timeStr)
@@ -58,28 +57,27 @@ func insertOrders(allPayments payments.AllPayments, data []string) {
 		quantity := splittedData[3]
 		quantityInt, err := strconv.Atoi(quantity)
 		if err != nil {
-			fmt.Printf("order (%d) insertion failed: quantity (%s) is not an integer\n", index, quantity)
-			return
+			return errors.New(fmt.Sprintf("order (%d) insertion failed: quantity (%s) is not an integer\n", index, quantity))
 		}
 		price := splittedData[4]
 		priceInt, err := parsePrice(price)
 		if err != nil {
-			fmt.Printf("order (%d) insertion failed: invalid price value (%s) \n", index, price)
-			return
+			return errors.New(fmt.Sprintf("order (%d) insertion failed: invalid price value (%s) \n", index, price))
 		}
 		dateFinal := dateStr + " " + timeStr
 		if err := allPayments.AddOrder(quantityInt, priceInt, item, dateFinal); err != nil {
-			fmt.Printf("order (%d) insertion failed: %s\n", index, err)
-			return
+			return errors.New(fmt.Sprintf("order (%d) insertion failed: %s\n", index, err))
 		} else {
 			okMsg = append(okMsg, fmt.Sprintf("successfully inserted order (%s, %s, %d, %s)\n", dateFinal, item, quantityInt, "TODO"))
 		}
 	}
 	fmt.Println(strings.Join(okMsg, ""))
+	return nil
 }
 
-func insertDetails(allPayments payments.AllPayments, data []string) {
+func insertDetails(allPayments payments.AllPayments, data []string) error {
 	for _, splittedData := range splitter(data) {
 		panic(splittedData)
 	}
+	return nil
 }
