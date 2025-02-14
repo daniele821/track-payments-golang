b603172bef817862258ffb4e9d5f2da66fc636ba
Daniele Muffato <danixgithub1@gmail.com>
2025-02-14 20:57:55 +0100

    move path detection code to main

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"payment/internal/client/cli"
 )
 
-const jsonPath string = "payments.json"
-
 func main() {
+	exePath, err := os.Executable()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	exePath, err = filepath.EvalSymlinks(exePath)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	jsonDir := filepath.Dir(exePath)
+	jsonPath := filepath.Join(append([]string{jsonDir}, "payments.json")...)
 	if err := cli.Run(jsonPath); err != nil {
 		fmt.Printf("%s\n", err)
+		return
 	}
 }
--- internal/client/cli/runner.go
+++ internal/client/cli/runner.go
@@ -1,34 +1,22 @@
 package cli
 
 import (
-	"os"
-	"path/filepath"
 	"payment/internal/server/payments"
 )
 
-func Run(jsonPathFromExeDir ...string) error {
-	// load json file
-	exePath, err := os.Executable()
-	if err != nil {
-		return err
-	}
-	exePath, err = filepath.EvalSymlinks(exePath)
-	if err != nil {
-		return err
-	}
-	jsonDir := filepath.Dir(exePath)
-	jsonPath := filepath.Join(append([]string{jsonDir}, jsonPathFromExeDir...)...)
-
+func Run(jsonPath string) error {
+	// load allPayments data
 	allPayments, err := payments.NewAllPaymentsFromjsonFile(jsonPath)
 	if err != nil {
 		return err
 	}
 
-	// execute program
+	// run program
 	if err := parseParamsAndRun(allPayments); err != nil {
 		return err
 	}
 
+	// save to json file
 	if err := allPayments.DumpJsonToFile(jsonPath, true); err != nil {
 		return err
 	}
