826e257de86c7dc954614ebb04c2c26a52e9f7d8
Daniele Muffato <danixgithub1@gmail.com>
2025-02-15 01:23:47 +0100

    added test

---
+++ internal/client/cli/box_drawing_test.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAlign(t *testing.T) {
+	expected := "London   "
+	actual := alignStr("London", 9, leftAlign)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("align failed: expected |%s|, actual |%s|", expected, actual)
+	}
+
+	expected = " London  "
+	actual = alignStr("London", 9, centerLeftAlign)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("align failed: expected |%s|, actual |%s|", expected, actual)
+	}
+
+	expected = "  London "
+	actual = alignStr("London", 9, centerRightAlign)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("align failed: expected |%s|, actual |%s|", expected, actual)
+	}
+
+	expected = "   London"
+	actual = alignStr("London", 9, rightAlign)
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("align failed: expected |%s|, actual |%s|", expected, actual)
+	}
+}
