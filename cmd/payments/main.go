358d174d7f30a5c9bb782d4507d8daf501aa09f3
Daniele Muffato <danixgithub1@gmail.com>
2025-02-16 08:55:04 +0100

    improved main file

--- cmd/payments/main.go
+++ cmd/payments/main.go
@@ -8,20 +8,36 @@ import (
 )
 
 func main() {
-	exePath, err := os.Executable()
+	args := os.Args[1:]
+
+	// print help message
+	if cli.ParseHelp(args) {
+		return
+	}
+
+	// get paths
+	jsonDir, err := getExeDir()
 	if err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
-	exePath, err = filepath.EvalSymlinks(exePath)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
-	jsonDir := filepath.Dir(exePath)
 	jsonPath := filepath.Join(append([]string{jsonDir}, "payments.json")...)
+
+	// run cli tool
 	if err := cli.Run(jsonPath); err != nil {
 		fmt.Printf("%s\n", err)
 		return
 	}
+}
+
+func getExeDir() (string, error) {
+	exePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	exePath, err = filepath.EvalSymlinks(exePath)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Dir(exePath), nil
 }
