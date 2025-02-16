52b927c80a557d8e5024d994119406eb1e174617
Daniele Muffato <danixgithub1@gmail.com>
2025-02-17 00:11:41 +0100

    changed readonly tree to better handle extremes

--- internal/server/payments/readonly_btree.go
+++ internal/server/payments/readonly_btree.go
@@ -22,32 +22,78 @@ func (readOnlyBTree ReadOnlyBTree[T]) Get(key T) (T, bool) {
 	return readOnlyBTree.btree.Get(key)
 }
 
-func (readOnlyBTree ReadOnlyBTree[T]) Ascend(iterator btree.ItemIteratorG[T], first, last *T) {
+func (readOnlyBTree ReadOnlyBTree[T]) Ascend(iterator btree.ItemIteratorG[T]) {
+	readOnlyBTree.btree.Ascend(iterator)
+}
+
+func (readOnlyBTree ReadOnlyBTree[T]) Descend(iterator btree.ItemIteratorG[T]) {
+	readOnlyBTree.btree.Descend(iterator)
+}
+
+func skipFirstItem[T any](item T, index *int, iterator btree.ItemIteratorG[T]) bool {
+	if *index == 0 {
+		return true
+	}
+	*index += 1
+	return iterator(item)
+}
+
+func (readOnlyBTree ReadOnlyBTree[T]) AscendRange(first, last *T, includeFirst, includeLast bool, iterator btree.ItemIteratorG[T]) {
 	switch {
 	case first == nil && last == nil:
 		readOnlyBTree.btree.Ascend(iterator)
 	case first == nil && last != nil:
 		readOnlyBTree.btree.AscendLessThan(*last, iterator)
 	case first != nil && last == nil:
-		readOnlyBTree.btree.AscendGreaterOrEqual(*first, iterator)
+		if includeFirst {
+			readOnlyBTree.btree.AscendGreaterOrEqual(*first, iterator)
+		} else {
+			index := 0
+			readOnlyBTree.btree.AscendGreaterOrEqual(*first, func(item T) bool { return skipFirstItem(item, &index, iterator) })
+		}
 	case first != nil && last != nil:
-		readOnlyBTree.btree.AscendRange(*first, *last, iterator)
+		if includeFirst {
+			readOnlyBTree.btree.AscendRange(*first, *last, iterator)
+		} else {
+			index := 0
+			readOnlyBTree.btree.AscendRange(*first, *last, func(item T) bool { return skipFirstItem(item, &index, iterator) })
+		}
 	default:
 		panic("UNREACHABLE CODE: all possible cases should have already been covered!")
 	}
+	if includeLast && last != nil {
+		if lastItem, exists := readOnlyBTree.Get(*last); exists {
+			iterator(lastItem)
+		}
+	}
 }
 
-func (readOnlyBTree ReadOnlyBTree[T]) Descend(iterator btree.ItemIteratorG[T], first, last *T) {
+func (readOnlyBTree ReadOnlyBTree[T]) DescendRange(first, last *T, includeFirst, includeLast bool, iterator btree.ItemIteratorG[T]) {
 	switch {
 	case first == nil && last == nil:
 		readOnlyBTree.btree.Descend(iterator)
 	case first == nil && last != nil:
 		readOnlyBTree.btree.DescendGreaterThan(*last, iterator)
 	case first != nil && last == nil:
-		readOnlyBTree.btree.DescendLessOrEqual(*first, iterator)
+		if includeFirst {
+			readOnlyBTree.btree.DescendLessOrEqual(*first, iterator)
+		} else {
+			index := 0
+			readOnlyBTree.btree.DescendLessOrEqual(*first, func(item T) bool { return skipFirstItem(item, &index, iterator) })
+		}
 	case first != nil && last != nil:
-		readOnlyBTree.btree.DescendRange(*first, *last, iterator)
+		if includeFirst {
+			readOnlyBTree.btree.DescendRange(*first, *last, iterator)
+		} else {
+			index := 0
+			readOnlyBTree.btree.DescendRange(*first, *last, func(item T) bool { return skipFirstItem(item, &index, iterator) })
+		}
 	default:
 		panic("UNREACHABLE CODE: all possible cases should have already been covered!")
+	}
+	if includeLast && last != nil {
+		if lastItem, exists := readOnlyBTree.Get(*last); exists {
+			iterator(lastItem)
+		}
 	}
 }
