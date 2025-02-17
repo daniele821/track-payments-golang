26995184614e2851e08779b6d012acfbff06f315
Daniele Muffato <danixgithub1@gmail.com>
2025-02-17 14:32:12 +0100

    fixed list payment|order

--- internal/client/cli/run_lister.go
+++ internal/client/cli/run_lister.go
@@ -52,11 +52,6 @@ func listPayments(data payments.ReadOnlyBTree[payments.Payment], from, to *strin
 			prevDate = date[:10]
 		}
 		fmt.Printf("%s | %s %s %s %d.%02d€\n", date, item.City(), item.Shop(), item.PaymentMethod(), price/100, price%100)
-		item.Orders().Ascend(func(item payments.Order) bool {
-			price := item.UnitPrice()
-			fmt.Printf("                 | %s x%d %d.%02d€\n", item.Item(), item.Quantity(), price/100, price%100)
-			return true
-		})
 		return true
 	})
 }
@@ -85,7 +80,7 @@ func listDetails(data payments.ReadOnlyBTree[payments.Payment], from, to *string
 		fmt.Printf("%s | %s %s %s %d.%02d€\n", date, item.City(), item.Shop(), item.PaymentMethod(), price/100, price%100)
 		item.Orders().Ascend(func(item payments.Order) bool {
 			price := item.UnitPrice()
-			fmt.Printf("                 | %s x%d %d.%02d€\n", item.Item(), item.Quantity(), price/100, price%100)
+			fmt.Printf("                 | x%d %s %d.%02d€\n", item.Quantity(), item.Item(), price/100, price%100)
 			return true
 		})
 		return true
