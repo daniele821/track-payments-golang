c636aeff8a2dce404830b9e4bfe22af11fde1bd2
Daniele Muffato <danixgithub1@gmail.com>
2025-02-17 13:59:49 +0100

    better visualize payments

--- internal/client/cli/run_visualize.go
+++ internal/client/cli/run_visualize.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"payment/internal/server/payments"
 	"strconv"
+	"time"
 )
 
 func visualizeGeneric(dataType string, data payments.ReadOnlyBTree[string], from, to *string) {
@@ -21,12 +22,28 @@ func visualizeGeneric(dataType string, data payments.ReadOnlyBTree[string], from
 
 func visualizePayment(data payments.ReadOnlyBTree[payments.Payment], from, to *string) {
 	fromPayment, toPayment, fromInclude, toInclude := strToPayment(from), strToPayment(to), true, true
-	boxData := [][][]string{{{"", "DATE", "CITY", "SHOP", "METHOD", "PRICE"}}}
+	boxData := [][][]string{{{"", "MONTH", "DAY", "TIME", "CITY", "SHOP", "METHOD", "PRICE"}}}
 	bodyData := [][]string{}
 	index := 0
+	monthOld, dayOld := "", ""
 	data.AscendRange(fromPayment, toPayment, fromInclude, toInclude, func(item payments.Payment) bool {
 		index += 1
-		bodyData = append(bodyData, []string{strconv.Itoa(index), item.Date(), item.City(), item.Shop(), item.PaymentMethod(), fmt.Sprintf("%.2f€", float64(item.TotalPrice())/100.0)})
+		dateTime, _ := time.Parse("2006/01/02 15:04", item.Date())
+		month, day, time := dateTime.Format("January 2006"), dateTime.Format("02"), dateTime.Format("15:04")
+		monthFmt, dayFmt, timeFmt := month, day, time
+		if dayOld != "" {
+			if month == monthOld {
+				monthFmt = ""
+			}
+			if day == dayOld {
+				dayFmt = ""
+			} else {
+				boxData = append(boxData, bodyData)
+				bodyData = [][]string{}
+			}
+		}
+		dayOld, monthOld = day, month
+		bodyData = append(bodyData, []string{strconv.Itoa(index), monthFmt, dayFmt, timeFmt, item.City(), item.Shop(), item.PaymentMethod(), fmt.Sprintf("%.2f€", float64(item.TotalPrice())/100.0)})
 		return true
 	})
 	boxData = append(boxData, bodyData)
