215e6bafe79b23dd3295ec6659ce8a7e5259fe56
Daniele Muffato <danixgithub1@gmail.com>
2025-02-18 05:06:14 +0100

    added aggregated visualizer

--- internal/client/cli/run_visualize.go
+++ internal/client/cli/run_visualize.go
@@ -102,5 +102,104 @@ func visualizeDetail(data payments.ReadOnlyBTree[payments.Payment], from, to *st
 }
 
 func visualizeAggregated(data payments.ReadOnlyBTree[payments.Payment], from, to *string) {
-	panic("TODO")
+	boxData := [][][]string{}
+	boxData = append(boxData, [][]string{{"PERIOD", "TOTAL", "PAYMENTS", "AVG-PAYMENT", "DAYS", "AVG-DAILY", "MIN", "MAX"}})
+	for _, row := range getAllAggregated(data, from, to) {
+		if row != nil {
+			boxData = append(boxData, [][]string{row})
+		}
+	}
+	fmt.Print(fmtBox(boxData, 1, 1, nil))
+}
+
+func getAggregated(data payments.ReadOnlyBTree[payments.Payment], name, from, to string) []string {
+	toMinute := to + " 99:99"
+	count := 0
+	totPrice := 0
+	minFound, maxFound := false, false
+	min, max := 0, 0
+	data.AscendRange(strToPayment(&from), strToPayment(&toMinute), true, false, func(item payments.Payment) bool {
+		count++
+		totPrice += item.TotalPrice()
+		if !minFound || min > item.TotalPrice() {
+			min = item.TotalPrice()
+			minFound = true
+		}
+		if !maxFound || max < item.TotalPrice() {
+			max = item.TotalPrice()
+			maxFound = true
+		}
+		return true
+	})
+	if count == 0 {
+		return nil
+	}
+	totPriceStr := strPrice(totPrice)
+	avg := fmt.Sprintf("%.2f€", float64(totPrice)/100.0/float64(count))
+	countStr := strconv.Itoa(count)
+	dayCount := days(from, to)
+	dayCountStr := strconv.Itoa(dayCount)
+	avgDaily := fmt.Sprintf("%.2f€", float64(totPrice)/100.0/float64(dayCount))
+	minStr := strPrice(min)
+	maxStr := strPrice(max)
+	return []string{name, totPriceStr, countStr, avg, dayCountStr, avgDaily, minStr, maxStr}
+}
+
+func days(from, to string) int {
+	fromDate, err := time.Parse("2006/01/02", from)
+	if err != nil {
+		panic(err)
+	}
+	toDate, err := time.Parse("2006/01/02", to)
+	if err != nil {
+		panic(err)
+	}
+	return int(toDate.Sub(fromDate).Hours()/24) + 1
+}
+
+func getAllAggregated(data payments.ReadOnlyBTree[payments.Payment], from, to *string) [][]string {
+	lines := [][]string{}
+
+	fromStr, toStr := "", ""
+	data.Ascend(func(item payments.Payment) bool {
+		fromStr = item.Date()[:10]
+		return false
+	})
+	data.Descend(func(item payments.Payment) bool {
+		toStr = item.Date()[:10]
+		return false
+	})
+
+	fromDate, err := time.Parse("2006/01/02", fromStr)
+	if err != nil {
+		panic(err)
+	}
+	toDate, err := time.Parse("2006/01/02", toStr)
+	if err != nil {
+		panic(err)
+	}
+	currentDate := fromDate
+
+	// monthly stats
+	for !currentDate.After(toDate) {
+		year, month, _ := currentDate.Date()
+		firstDayMonth := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+		nextMonth := month + 1
+		nextYear := year
+		if nextMonth > time.December {
+			nextMonth = time.January
+			nextYear++
+		}
+		firstDayOfNextMonth := time.Date(nextYear, nextMonth, 1, 0, 0, 0, 0, time.UTC)
+		lastDayMonth := firstDayOfNextMonth.AddDate(0, 0, -1)
+
+		lines = append(lines, getAggregated(data, firstDayMonth.Format("2006 January"), firstDayMonth.Format("2006/01/02"), lastDayMonth.Format("2006/01/02")))
+
+		currentDate = firstDayOfNextMonth
+	}
+
+	// complessive stats
+	lines = append(lines, getAggregated(data, "TOTAL", fromStr, toStr))
+
+	return lines
 }
