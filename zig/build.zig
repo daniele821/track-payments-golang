4a5d967ba981ddd023832791f41c0b0a283b9714
Daniele Muffato <danixgithub1@gmail.com>
2025-03-11 09:01:30 +0100

    updated build file

--- zig/build.zig
+++ zig/build.zig
@@ -7,10 +7,8 @@ pub fn build(b: *std.Build) void {
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
-    const run_exe = b.addRunArtifact(exe);
-
     b.installArtifact(exe);
 
     const run_step = b.step("run", "run the program");
-    run_step.dependOn(&run_exe.step);
+    run_step.dependOn(&b.addRunArtifact(exe).step);
 }
