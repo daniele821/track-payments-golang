c542866d42ea05ce583d07e6611ca9f4f78ee9f2
Daniele Muffato <danixgithub1@gmail.com>
2025-03-11 14:38:28 +0100

    shell runner kinda working

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -3,8 +3,7 @@ const std = @import("std");
 pub fn main() !void {
     std.debug.print("testing\n", .{});
 
-    const strings = [_][]const u8{ "echo", "ciao", "" };
+    const strings = ([_][]const u8{ "echo", "ciao", "FUCKER!" })[0..];
 
-    const slice_of_slices: []const []const u8 = strings[0..];
-    try @import("./utils/shell.zig").runCmd(std.heap.page_allocator, slice_of_slices);
+    try @import("./utils/shell.zig").runCmd(std.heap.page_allocator, strings);
 }
--- zig/src/utils/shell.zig
+++ zig/src/utils/shell.zig
@@ -2,6 +2,15 @@ const std = @import("std");
 
 pub fn runCmd(allocator: std.mem.Allocator, cmd: []const []const u8) !void {
     var child = std.process.Child.init(cmd, allocator);
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+
     try child.spawn();
-    _ = try child.wait();
+
+    const stdout = try child.stdout.?.readToEndAlloc(allocator, std.math.maxInt(usize));
+    const stderr = try child.stderr.?.readToEndAlloc(allocator, std.math.maxInt(usize));
+
+    const term = try child.wait();
+
+    std.debug.print("{s}{s}{}\n", .{ stdout, stderr, term });
 }
