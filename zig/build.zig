1b3403ddacd8b19d34a7852e774ad8448a442064
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 11:12:55 +0100

    renamed file to make navigation easier

--- zig/build.zig
+++ zig/build.zig
@@ -17,7 +17,7 @@ pub fn build(b: *std.Build) void {
     // add a test step
     const test_step = b.step("test", "Run the tests");
     const tests = b.addTest(.{
-        .root_source_file = b.path("src/unit_tests.zig"),
+        .root_source_file = b.path("src/tests.zig"),
     });
     test_step.dependOn(&b.addRunArtifact(tests).step);
 }
