94aaba7920fa6c9b7f15932ec1afda7a30c52b4e
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 12:01:29 +0100

    todo crypto signatures

--- zig/src/utils/crypto.zig
+++ zig/src/utils/crypto.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
-const keyBytes = 256 / 8;
+const blockSize: usize = 32;
 
-// pub fn encrypt(key: [keyBytes]u8) void {
-//     std.crypto.core.aes.Aes256.initEnc(key).encryptWide(comptime count: usize, dst: *[16*count]u8, src: *const [16*count]u8)
-// }
+pub fn encrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []u8) []u8 {
+    _ = allocator;
+    _ = key;
+    _ = message;
+}
+
+pub fn decrypt(allocator: std.mem.Allocator, key: [blockSize]u8, message: []u8) []u8 {
+    _ = allocator;
+    _ = key;
+    _ = message;
+}
