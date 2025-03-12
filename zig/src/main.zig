33f30b7b9e8f5eb47a6aadc55c95acdd6416bb59
Daniele Muffato <danixgithub1@gmail.com>
2025-03-12 11:11:54 +0100

    removed shell runner function, as encryption can be done simply via stdlib

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -2,9 +2,4 @@ const std = @import("std");
 
 pub fn main() !void {
     std.debug.print("testing\n", .{});
-
-    const strings = ([_][]const u8{ "echo", "CMD OUTPUT" })[0..];
-
-    const res = try @import("./utils/shell.zig").runCmd(std.heap.page_allocator, strings, .{});
-    res.deinit();
 }
--- zig/src/utils/crypto.zig
+++ zig/src/utils/crypto.zig
@@ -1,1 +1,7 @@
 const std = @import("std");
+
+const keyBytes = 256 / 8;
+
+// pub fn encrypt(key: [keyBytes]u8) void {
+//     std.crypto.core.aes.Aes256.initEnc(key).encryptWide(comptime count: usize, dst: *[16*count]u8, src: *const [16*count]u8)
+// }
