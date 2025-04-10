6032e2d61c15e241eb8fe2a14563e1d6ff5432e2
Daniele Muffato <danixgithub1@gmail.com>
2025-04-10 21:41:53 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRIPT_PWD="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${SCRIPT_PWD}")"
+
+cd "$SCRIPT_DIR" || exit 1
+
+GOMAIN_PATH="${SCRIPT_DIR}/cmd/payments/main.go"
+
+if [[ "$#" -eq "1" ]]; then
+    BIN_PATH="$1"
+else
+    echo 'invalid amount of parameters'
+    exit 1
+fi
+
+# automagically build go executable ONLY if go source code was updated in the meanwhile
+if [[ -f "$BIN_PATH" ]]; then
+    latest_go_file=$(find "$SCRIPT_DIR" -type f -name "*.go" -printf "%T@ %p\n" | sort -nr | head -n1 | awk '{print $2}')
+    other_file="$BIN_PATH"
+
+    if [[ $(stat -c %Y "$latest_go_file") -gt $(stat -c %Y "$other_file") ]]; then
+        echo 'compiling program...'
+        [[ -f "$BIN_PATH" ]] && rm "$BIN_PATH"
+        go build -o "$BIN_PATH" "$GOMAIN_PATH"
+    fi
+else
+    go build -o "$BIN_PATH" "$GOMAIN_PATH"
+fi
