965cf3fed000bd5a696f5d9c5f84505752ca999e
Daniele Muffato <danixgithub1@gmail.com>
2025-04-12 10:24:04 +0200

    simplify build code

--- build.sh
+++ build.sh
@@ -14,17 +14,5 @@ else
     exit 1
 fi
 
-# automagically build go executable ONLY if go source code was updated in the meanwhile
-if [[ -f "$BIN_PATH" ]]; then
-    latest_go_file=$(find "$SCRIPT_DIR" -type f -name "*.go" -printf "%T@ %p\n" | sort -nr | head -n1 | awk '{print $2}')
-    other_file="$BIN_PATH"
-
-    if [[ $(stat -c %Y "$latest_go_file") -gt $(stat -c %Y "$other_file") ]]; then
-        echo 'compiling program (payments) ...'
-        [[ -f "$BIN_PATH" ]] && rm "$BIN_PATH"
-        go build -o "$BIN_PATH" "$GOMAIN_PATH"
-    fi
-else
-    echo 'compiling program (payments) ...'
-    go build -o "$BIN_PATH" "$GOMAIN_PATH"
-fi
+# build executable binary
+go build -o "$BIN_PATH" "$GOMAIN_PATH"
